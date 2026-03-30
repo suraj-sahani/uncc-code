8ee0fd6ae3cabb534c7f3a8f54001e069301edcf
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-30 12:50:52 +0530

    feat : Add storing negative number to buffers - non-traditional way

--- src/buffer/buffer_1.js
+++ src/buffer/buffer_1.js
@@ -2,16 +2,16 @@ import { Buffer } from 'node:buffer'
 
 const memoryContainer = Buffer.alloc(4)
 
-console.log(memoryContainer)
-console.log(memoryContainer[0])
-
 memoryContainer[0] = 0xf4
 memoryContainer[1] = 0x34
 memoryContainer[2] = 0xb6
 memoryContainer[3] = 0xff
 
-console.log(memoryContainer)
-console.log(memoryContainer[0])
-console.log(memoryContainer[1])
-console.log(memoryContainer[2])
-console.log(memoryContainer[3])
+// We have seen that can store positive number in a buffer
+// but what is we store a negative number?
+
+memoryContainer[0] = -32 // => <Buffer e0 34 b6 ff>
+console.log(memoryContainer[0]) // => 224
+
+// Why did we get a large number i.e 224 for -32
+// Because 224 is nothing but the 2's complement of 34
