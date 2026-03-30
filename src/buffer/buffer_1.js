cf975d6ba36d7889c8ec8384cfe242269e7671dd
suraj-sahani <suraj.sahani.961@gmail.com>
2026-03-30 13:28:54 +0530

    feat: Add Intro to buffers challenge

--- src/buffer/buffer_1.js
+++ src/buffer/buffer_1.js
@@ -1,17 +1,28 @@
-import { Buffer } from 'node:buffer'
+import { Buffer } from "node:buffer";
 
-const memoryContainer = Buffer.alloc(4)
+const memoryContainer = Buffer.alloc(4);
 
-memoryContainer[0] = 0xf4
-memoryContainer[1] = 0x34
-memoryContainer[2] = 0xb6
-memoryContainer[3] = 0xff
+memoryContainer[0] = 0xf4;
+memoryContainer[1] = 0x34;
+memoryContainer[2] = 0xb6;
+memoryContainer[3] = 0xff;
 
 // We have seen that can store positive number in a buffer
 // but what is we store a negative number?
 
-memoryContainer[0] = -32 // => <Buffer e0 34 b6 ff>
-console.log(memoryContainer[0]) // => 224
-
+memoryContainer[0] = -32; // => <Buffer e0 34 b6 ff>
+console.log(memoryContainer[0]); // => 224
 // Why did we get a large number i.e 224 for -32
 // Because 224 is nothing but the 2's complement of 34
+// Non-traditional way to store negative number in Buffers.
+
+// If we want to store negative integers, we use the writeInt8 method
+// It takes two arguments, 1 : negative interger, 2 : offset/postion in the buffer,
+// where we want to store this number
+memoryContainer.writeInt8(-34, 2);
+// We also use the method readInt8 to get the exact negative value instead of the 2's complement value
+console.log(memoryContainer.readInt8(2)); // => -34
+
+// To get all the values in the buffer logged,
+// we can use the toString() method
+console.log(memoryContainer.toString("hex"));
---
+++ src/buffer/challenge_1.js
@@ -0,0 +1,20 @@
+import { Buffer } from "node:buffer";
+
+// Allocate just the exact size of buffer required to store the data below to memory
+// 0100 1000 0110 1001 0010 0001
+// Once it is allocated, log it in utf-8 encoding
+
+const binaryRep = ["0100", "1000", "0110", "1001", "0010", "0001"]; // 6 items each of 4 bits => Total = 24 bits ~ 3 bytes
+// Since we allocate memory in terms of 8bits, we merge each pair into 1
+// 0100 1000 => 0x48
+// 0110 1001 => 0x69
+// 0110 0001 => 0x21
+const descimalRep = [0x48, 0x69, 0x21];
+
+const allocatedBuffer = Buffer.alloc(3);
+
+descimalRep.forEach((num, index) => {
+  allocatedBuffer[index] = num;
+});
+
+console.log(allocatedBuffer.toString("utf-8")); // => Output : Hi!
