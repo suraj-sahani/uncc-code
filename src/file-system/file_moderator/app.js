686b1f10acbb2d83eb553bedb145abb8b2108d88
suraj-sahani <suraj.sahani.961@gmail.com>
2026-04-23 21:10:21 +0530

    feat: migrate to event emitter for detecting file changes

--- src/file-system/file_moderator/app.js
+++ src/file-system/file_moderator/app.js
@@ -3,8 +3,52 @@ import fs from "fs/promises";
 (async () => {
   try {
     const commandFile = await fs.open("./command.txt", "r")
+
+    // The FileHandle object that we get from fs.open()
+    // is an EventEmitter as well, so, we can use this to clean-up our code
+    commandFile.on('change', async () => {
+      // To read the contents of a file, we have to first open it.
+      // When we open a file, we are not actually moving all the content of the file
+      // to the memory, except, we are saving a number regaring the file.
+      // This number is called the file descriptor.
+      // const content = await commandFile.read()
+      // When we log this file, we can see that the amount of bytes allocated,
+      // for this is much more that actually needed.
+      // This is overuse of memory.
+      // console.dir({ content }, { depth: Infinity })
+
+      // to allocate a buffer that is only limmited to content,
+      // we need to pass an option to  the readFile method
+      // this will be size of the bufffer and to allocate that size,
+      // we pass it using Buffer.alloc()
+      // But, we need to find out the size of out file.
+      const fileStats = await commandFile.stat()
+      const fileSize = fileStats.size
+      // Now, the memory allocated is only limited to the size of the file/contents
+      // of the file
+      // const content = await commandFile.read(Buffer.alloc(fileSize))
+      // We can often see that we are getting logs where the file/buffer is filled with zeros.
+      // This is due to the fact that everytime we save the file, the position from which,
+      // the file is read changes
+      // this is due to the fact that when the file is read for the first time, it reads it from the beginning
+      // but afterwards, it reads it from the end. This is why we get all zeros as there is nothing
+      // after the end of the file contents.
+      // We can prevent this by passing the exact location to the file,
+      // in the fileRead method.
+      // Reference: https://nodejs.org/docs/latest/api/fs.html#filehandlereadfileoptions
+      // Syntax = command.readFile(buffer,offset,length, options)
+      const offset = 0, buff = Buffer.alloc(fileSize), length = buff.byteLength, position = 0
+      // Now, we always get the correct refernce to the file
+      const content = await commandFile.read(buff, offset, length, position)
+      console.dir({ content }, { depth: Infinity })
+
+
+    })
+
     // Watch the entire directory for changes
     const watcher = fs.watch("./command.txt");
+
+
 
     for await (const event of watcher) {
       // The event value return the following results:
@@ -16,45 +60,11 @@ import fs from "fs/promises";
       // and sometimes, there can be multiple logs.
       // There is no definite reason for it and is dependent
       // on how the OS handles file changes
-
-
       // We need to check for change events only.
       if (event.eventType === "change") {
-        // To read the contents of a file, we have to first open it.
-        // When we open a file, we are not actually moving all the content of the file
-        // to the memory, except, we are saving a number regaring the file.
-        // This number is called the file descriptor.
-        // const content = await commandFile.read()
-        // When we log this file, we can see that the amount of bytes allocated,
-        // for this is much more that actually needed.
-        // This is overuse of memory.
-        // console.dir({ content }, { depth: Infinity })
-
-        // to allocate a buffer that is only limmited to content,
-        // we need to pass an option to  the readFile method
-        // this will be size of the bufffer and to allocate that size,
-        // we pass it using Buffer.alloc()
-        // But, we need to find out the size of out file.
-        const fileStats = await commandFile.stat()
-        const fileSize = fileStats.size
-        // Now, the memory allocated is only limited to the size of the file/contents
-        // of the file
-        // const content = await commandFile.read(Buffer.alloc(fileSize))
-        // We can often see that we are getting logs where the file/buffer is filled with zeros.
-        // This is due to the fact that everytime we save the file, the position from which,
-        // the file is read changes
-        // this is due to the fact that when the file is read for the first time, it reads it from the beginning
-        // but afterwards, it reads it from the end. This is why we get all zeros as there is nothing
-        // after the end of the file contents.
-        // We can prevent this by passing the exact location to the file,
-        // in the fileRead method.
-        // Reference: https://nodejs.org/docs/latest/api/fs.html#filehandlereadfileoptions
-        // Syntax = command.readFile(buffer,offset,length, options)
-        const offset = 0, buff = Buffer.alloc(fileSize), length = fileSize, position = 0
-        // Now, we always get the correct refernce to the file
-        const content = await commandFile.read(buff, offset, length, position)
-        console.dir({ content }, { depth: Infinity })
-
+        // We emit the change event and let the EventEmitter callback,
+        // handle our logic
+        commandFile.emit('change')
       }
 
     }
