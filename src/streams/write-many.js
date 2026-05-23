a17bd5b97ebb9b5d72698bb522e7178e9246e358
suraj-sahani <suraj.sahani.961@gmail.com>
2026-05-24 00:01:46 +0530

    feat: Add initial apporach to using streams

--- src/streams/write-many.js
+++ src/streams/write-many.js
@@ -1,6 +1,5 @@
-import fs from "node:fs"
-import { performance } from "perf_hooks"
 
+// import fs from "fs/promises"
 // Execution Time : 8s
 // CPU Usage : 100% of 1 core
 // Memory Usage: 50mb
@@ -23,6 +22,7 @@ import { performance } from "perf_hooks"
 // })()
 
 
+// import fs from "node:fs"
 // Execution Time: 0.6s
 // CPU Usage: 100% of 1 core
 // Memory Usage: 600mb
@@ -43,24 +43,45 @@ import { performance } from "perf_hooks"
 //
 // })()
 
-
+// import fs from "node:fs"
 // Execution Time: 1.3s
 // CPU Usage: 100% of 1 core
 // Memory Usage: 30mb
+// (async () => {
+//
+//   console.time("write_many")
+//   fs.open("./test-write-many.txt", "w", (err, fd) => {
+//
+//
+//     for (let i = 0; i < 1_000_000; i++) {
+//       fs.writeSync(fd, `Iteration count: ${i}\n`)
+//     }
+//
+//     console.timeEnd("write_many")
+//
+//   })
+//
+// })()
+
+import fs from "fs/promises"
+// We can make this process much faster using streams
+// Note: This is a bad way of doing it
+// One of the problems is the memory usage
+// We will optimise it later
+// Execution Time: 0.15s/150ms
+// CPU Usage: 100% of 1 core
+// Memory Usage: 200mb
 (async () => {
+  console.time("write_stream")
 
-  console.time("write_many")
-  fs.open("./test-write-many.txt", "w", (err, fd) => {
+  const fileHandle = await fs.open("./test-write-many.txt", "w")
 
+  const stream = fileHandle.createWriteStream()
 
-    for (let i = 0; i < 1_000_000; i++) {
-      fs.writeSync(fd, `Iteration count: ${i}\n`)
-    }
+  for (let i = 0; i < 1_000_000; i++) {
+    const buffer = Buffer.from(`Iteration: ${i}\n`, "utf-8")
+    stream.write(buffer)
+  }
 
-    console.timeEnd("write_many")
-
-  })
-
-
+  console.timeEnd("write_stream")
 })()
-
